610d61867c39e14d7ff1c6485fb37bcda51fff70
Shantanav Saurav <ss9415@rit.edu>
2021-03-23 18:14:31 -0400

    Bot will launch and print a ready message now

--- DiscordCCBot.py
+++ DiscordCCBot.py
@@ -5,3 +5,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord.')
+
+if __name__ == "__main__":
+    client.run(TOKEN)
+
