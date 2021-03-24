5a058734ec4468a58312dbe0ff066a0618399a81
Shantanav Saurav <ss9415@rit.edu>
2021-03-23 20:02:42 -0400

    Bot will now join vc, but won't leave until it times out

--- DiscordCCBot.py
+++ DiscordCCBot.py
@@ -1,5 +1,7 @@
 import os
 import discord
+from discord.ext import commands
+from discord.voice_client import VoiceClient
 import speech_recognition as sr
 from dotenv import load_dotenv
 
@@ -10,6 +12,16 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user: return # Don't make the bot reply to itself
+
+    if message.content.startswith("!join"):
+        if message.author.voice:
+            vc = await message.author.voice.channel.connect()
+        else:
+            await message.channel.send("You're not in a voice channel idiot")
 
 if __name__ == "__main__":
     client.run(TOKEN)
