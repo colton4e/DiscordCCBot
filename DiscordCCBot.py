6da8baf2a3610176580ffadaebf1963da75cde51
Colton Forry <colton4e@gmail.com>
2021-03-27 22:19:52 -0400

    Added leave command
    Co-Authored-By: Shantanav Saurav <ss9415@rit.edu>

--- DiscordCCBot.py
+++ DiscordCCBot.py
@@ -8,21 +8,37 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
 client = discord.Client()
+VC = None
+
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord.')
 
+
 @client.event
 async def on_message(message):
-    if message.author == client.user: return # Don't make the bot reply to itself
+    global VC
+    if message.author == client.user: return  # Don't make the bot reply to itself
 
     if message.content.startswith("!join"):
         if message.author.voice:
-            vc = await message.author.voice.channel.connect()
+            VC = await message.author.voice.channel.connect()
+            await message.channel.send(f'Joined voice channel **{message.author.voice.channel}**.')
         else:
             await message.channel.send("You're not in a voice channel idiot")
 
+    if message.content.startswith("!leave"):
+        if VC is None:
+            await message.channel.send("I'm not in a voice channel idiot")
+            return
+
+        if message.author.voice:
+            VC = await VC.disconnect()
+            await message.channel.send(f'Left voice channel **{message.author.voice.channel}**.')
+        else:
+            await message.channel.send("You're not in a voice channel idiot")
+
+
 if __name__ == "__main__":
     client.run(TOKEN)
-
