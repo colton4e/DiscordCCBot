78aa47058076f0eb26685b9aa1ca1ca0ff7adb17
Colton Forry <colton4e@gmail.com>
2021-03-27 23:07:05 -0400

    Removed extra newlines

--- DiscordCCBot.py
+++ DiscordCCBot.py
@@ -10,11 +10,9 @@ TOKEN = os.getenv('BOT_TOKEN')
 client = discord.Client()
 VC = None
 
-
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord.')
-
 
 @client.event
 async def on_message(message):
@@ -38,7 +36,6 @@ async def on_message(message):
             await message.channel.send(f'Left voice channel **{message.author.voice.channel}**.')
         else:
             await message.channel.send("You're not in a voice channel idiot")
-
 
 if __name__ == "__main__":
     client.run(TOKEN)
