fdd5d2f49cec5450db133270914e6017efc938de
Colton Forry <colton4e@gmail.com>
2021-03-29 12:40:23 -0400

    Reworked an if statement and added print statements

--- DiscordCCBot.py
+++ DiscordCCBot.py
@@ -1,4 +1,5 @@
 import os
+
 import discord
 from discord.ext import commands
 from discord.voice_client import VoiceClient
@@ -10,9 +11,11 @@ TOKEN = os.getenv('BOT_TOKEN')
 client = discord.Client()
 VC = None
 
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord.')
+
 
 @client.event
 async def on_message(message):
@@ -23,6 +26,7 @@ async def on_message(message):
         if message.author.voice:
             VC = await message.author.voice.channel.connect()
             await message.channel.send(f'Joined voice channel **{message.author.voice.channel}**.')
+            print(f'Joined {message.author.voice.channel.id} with session ID {message.author.voice.session_id}')
         else:
             await message.channel.send("You're not in a voice channel idiot")
 
@@ -30,12 +34,13 @@ async def on_message(message):
         if VC is None:
             await message.channel.send("I'm not in a voice channel idiot")
             return
-
-        if message.author.voice:
+        elif message.author.voice:
             VC = await VC.disconnect()
             await message.channel.send(f'Left voice channel **{message.author.voice.channel}**.')
+            print('Left the connected voice channel')
         else:
             await message.channel.send("You're not in a voice channel idiot")
+
 
 if __name__ == "__main__":
     client.run(TOKEN)
